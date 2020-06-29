b5a5f26056c3fc0cf88551c37e623290e261244e
Pierluigi <androide79@gmail.com>
2020-06-30 00:32:08 +0200

    logger

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/mitchellh/mapstructure v1.3.2 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/pelletier/go-toml v1.8.0 // indirect
+	github.com/sirupsen/logrus v1.2.0
 	github.com/smartystreets/assertions v1.1.1 // indirect
 	github.com/spf13/afero v1.3.0 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
--- play.go
+++ play.go
@@ -2,19 +2,44 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	greetings "github.com/Androide79/go-greetings"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
 func main() {
+
+	// init logger function
+	initLogger()
+
 	// set config file path
 	viper.SetConfigFile(".env")
 	// read config file
 	viper.ReadInConfig()
 
 	var name string = viper.GetString("NAME")
+	log.Infof("'name' variable init with '%v' value", name)
 	var lang string = viper.GetString("LANGUAGE")
+	log.Infof("'lang' variable init with '%v' value", lang)
 
 	fmt.Println(greetings.Greet(name, lang))
+}
+
+func initLogger() {
+
+	// choose logger file
+	file, err := os.OpenFile("play.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// set log output to file
+	log.SetOutput(file)
+	// set the formatter (if you consider moving these data to a centralized system you can use 'JSONFormatter')
+	log.SetFormatter(&log.TextFormatter{})
+	// set log level
+	log.SetLevel(log.InfoLevel)
 }
